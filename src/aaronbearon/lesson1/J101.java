007042798845ea53df709caaa86bc5b9d5f3f7df
Aaron Blum <googolpup@gmail.com>
2025-08-28 17:24:31 -0400

    Add j101

---
+++ src/aaronbearon/lesson1/J101.java
@@ -0,0 +1,8 @@
+package aaronbearon.lesson1;
+
+public class J101 {
+    public static void main(String[] args) {
+        System.out.println("Aaron M. Blum");
+        System.out.println(99);
+    }
+}
