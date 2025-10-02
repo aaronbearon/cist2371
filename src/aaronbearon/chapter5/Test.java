40cebd4028eff767968723e45c26a92d62d9bef2
Aaron Blum <googolpup@gmail.com>
2025-10-02 14:14:48 -0400

    Interview 5 attempt done. The Test file I found should have a statement to the user on top.

---
+++ src/aaronbearon/chapter5/Test.java
@@ -0,0 +1,9 @@
+package aaronbearon.chapter5;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 15; i++) {
+            System.out.printf("Pi (%d): %." + i + "f\n", i, Math.PI);
+        }
+    }
+}
