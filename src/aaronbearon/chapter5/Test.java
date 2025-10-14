af136e5208a2cd37666b6e1c3c52d9394f85389f
Aaron Blum <googolpup@gmail.com>
2025-10-14 19:46:32 -0400

    Fixed a little format

--- src/aaronbearon/chapter5/Test.java
+++ src/aaronbearon/chapter5/Test.java
@@ -3,7 +3,7 @@ package aaronbearon.chapter5;
 public class Test {
     public static void main(String[] args) {
         for (int i = 2; i <= 15; i++) {
-            System.out.printf("Pi (%d): %." + i + "f\n", i, Math.PI);
+            System.out.printf("Pi (%2d): %." + i + "f%n", i, Math.PI);
         }
     }
 }
