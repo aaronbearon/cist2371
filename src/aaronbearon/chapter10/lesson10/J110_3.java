2aa9836ece0f8e73c1c4163fe73ce3a392984de9
Aaron Blum <googolpup@gmail.com>
2025-11-14 17:02:04 -0500

    Part 2 of lab 10

---
+++ src/aaronbearon/chapter10/lesson10/J110_3.java
@@ -0,0 +1,62 @@
+package aaronbearon.chapter10.lesson10;
+
+import java.math.BigDecimal;
+
+import java.math.RoundingMode;
+
+public class J110_3 {
+    public static void main(String[] args) {
+        int[] preciseNums = {1, 2, 5, 10, 50, 100, 200};
+        Euler euler = new Euler();
+        for (int i = 1; i < preciseNums.length; i++) {
+            euler.setPrecision(preciseNums[i]);
+            System.out.printf("Euler precision %d: %.20f%n", euler.getPrecision(), euler.getEuler());
+        }
+    }
+}
+
+class Euler {
+    private int precision;
+
+    Euler() {
+        this(1);
+    }
+
+    Euler(int precision) {
+        this.precision = precision;
+    }
+
+    void setPrecision(int precision) {
+        this.precision = precision;
+    }
+
+    int getPrecision() {
+        return precision;
+    }
+
+    BigDecimal getEuler() {
+        return calculateEuler();
+    }
+
+    private BigDecimal calculateEuler() {
+        BigDecimal euler = new BigDecimal("0.0");
+        for (int i = 0; i <= precision; i++) {
+            if (i == 0) {
+                euler = euler.add(BigDecimal.valueOf(1));
+            } else {
+                euler = euler.add(BigDecimal.valueOf(1).divide((getFactorial(i)), 20, RoundingMode.HALF_UP));
+            }
+        }
+
+        return euler;
+    }
+
+    private BigDecimal getFactorial(int i) {
+        BigDecimal factors = new BigDecimal("1.0");
+        for (int j = 1; j <= i; j++) {
+            factors = factors.multiply(BigDecimal.valueOf(j));
+        }
+
+        return factors;
+    }
+}
