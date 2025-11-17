e0ac54e04e872e69c8fca446dfc875a100f86cdf
Aaron Blum <googolpup@gmail.com>
2025-11-16 19:55:58 -0500

    Lab 10 done!

--- src/aaronbearon/chapter10/lesson10/J110_2.java
+++ src/aaronbearon/chapter10/lesson10/J110_2.java
@@ -1,62 +1,72 @@
 package aaronbearon.chapter10.lesson10;
 
-// Use kilograms/meter and pounds/inch.
-
-// User decides which.
-
-// Calculate BMI : kilogram weight divided by the square of meter height.
+/**
+ * Aaron Blum
+ * Java Lab Part 2
+ * 2025-11-16
+ */
 public class J110_2 {
+    /**
+     * Test out the BMI class with some examples.
+     */
     public static void main(String[] args) {
         //* Smart sample data
         BMI person1 = new BMI(170, 75, false);
-        System.out.println("BMI is " + person1.getBMI() +
-                ", which is " + person1.getStatus() + ".");
+        System.out.printf("Your BMI is %.2f which is %s.%n", person1.getBMI(), person1.getStatus());
         BMI person2 = new BMI(73, 2.5, true);
-        System.out.println("BMI is " + person2.getBMI() +
-                ", which is " + person2.getStatus() + ".");
+        System.out.printf("Your BMI is %.2f which is %s.%n", person2.getBMI(), person2.getStatus());
         BMI person3 = new BMI(220.2, 56.3, false);
-        System.out.println("BMI is " + person3.getBMI() +
-                ", which is " + person3.getStatus() + ".");
+        System.out.printf("Your BMI is %.2f which is %s.%n", person3.getBMI(), person3.getStatus());
         BMI person4 = new BMI(109.5, 2, true);
-        System.out.println("BMI is " + person4.getBMI() +
-                ", which is " + person4.getStatus() + ".");
+        System.out.printf("Your BMI is %.2f which is %s.%n", person4.getBMI(), person4.getStatus());
     }
 }
 
-//* Immutable class
+/**
+ * BMI helps calculate a person's body mass index and whether they are healthy or not.
+ */
 class BMI {
-    //* These are fixed once the object is created.
-    //* Can't access them directly
-    private final double weight; // in pounds
-    private final double height; // in inches
-    private final boolean isMetric;
+    //* Remove unused fields; don't need the name or age for this problem.
+    //* Made class immutable.
+    private final double weight; // in pounds or kilograms, depending on isMetric
+    private final double height; // in inches or meters, depending on isMetric
+    private final boolean isMetric; //* New field to track metric vs. english units.
 
-    //* Flexible value, but no direct access
-    private double bmi;
+    // Conversion constants.
+    private static final double KILOGRAMS_PER_POUND = 0.45359237;
+    private static final double METERS_PER_INCH = 0.0254;
 
-    //* Safe for direct access
-    static final double KILOGRAMS_PER_POUND = 0.45359237;
-    static final double METERS_PER_INCH = 0.0254;
-
-    BMI(double weight, double height, boolean isMetric) {
+    /**
+     * BMI constructs a new object.
+     *
+     * @param weight   in Kilograms or Pounds
+     * @param height   in Meters or Inches
+     * @param isMetric Sets kilograms/meter or pounds/inch
+     */
+    public BMI(double weight, double height, boolean isMetric) {
         this.weight = weight;
         this.height = height;
         this.isMetric = isMetric;
     }
 
-    double getBMI() {
+    /**
+     * Returns the person's BMI : kilogram weight divided by the square of meter height.
+     */
+    public double getBMI() {
         //* Combines the two converters into one simple selection.
         if (isMetric) {
-            bmi = weight / Math.pow(height, 2);
+            return weight / Math.pow(height, 2); //* use Math.pow
         } else {
-            bmi = weight * KILOGRAMS_PER_POUND / Math.pow(height * METERS_PER_INCH, 2);
+            // Convert pounds/inch^2 to kilograms/meter^2.
+            return weight * KILOGRAMS_PER_POUND / Math.pow(height * METERS_PER_INCH, 2); //* use Math.pow
         }
-
-        return Math.round(bmi * 100) / 100.0;
     }
 
-    String getStatus() {
-        //* Same as before
+    /**
+     * Returns the person's BMI description.
+     */
+    public String getStatus() {
+        double bmi = getBMI();
         if (bmi < 18.5) {
             return "Underweight";
         } else if (bmi < 25) {
--- src/aaronbearon/chapter10/lesson10/J110_3.java
+++ src/aaronbearon/chapter10/lesson10/J110_3.java
@@ -4,7 +4,15 @@ import java.math.BigDecimal;
 
 import java.math.RoundingMode;
 
+/**
+ * Aaron Blum
+ * Java Lab Part 3
+ * 2025-11-16
+ */
 public class J110_3 {
+    /**
+     * Test Euler with sample precisions
+     */
     public static void main(String[] args) {
         int[] preciseNums = {1, 2, 5, 10, 50, 100, 200};
         Euler euler = new Euler();
@@ -15,28 +23,56 @@ public class J110_3 {
     }
 }
 
+/**
+ * Euler helps approximate Euler's number.
+ */
 class Euler {
+    /**
+     * Number of terms to add for approximation
+     */
     private int precision;
 
+    /**
+     * Prevents rewriting complex code
+     */
     private static final BigDecimal ONE = BigDecimal.valueOf(1);
 
-    Euler() {
+    /**
+     * No arg constructor
+     */
+    public Euler() {
         this(1);
     }
 
-    Euler(int precision) {
+    /**
+     * Constructor which accepts the number of terms
+     *
+     * @param precision Highest factorial number
+     */
+    public Euler(int precision) {
         this.precision = precision;
     }
 
-    void setPrecision(int precision) {
+    /**
+     * Sets the precision
+     *
+     * @param precision Highest factorial number
+     */
+    public void setPrecision(int precision) {
         this.precision = precision;
     }
 
-    int getPrecision() {
+    /**
+     * Get the number of terms in the calculation.
+     */
+    public int getPrecision() {
         return precision;
     }
 
-    BigDecimal getEuler() {
+    /**
+     * Computes approximate euler with the given number of terms
+     */
+    public BigDecimal getEuler() {
         BigDecimal euler = ONE;
         BigDecimal divisor = ONE;
         for (int i = 1; i <= precision; i++) {
