ab8df183c78b66c53029f918b9057277ca5b827e
Aaron Blum <googolpup@gmail.com>
2025-11-27 22:28:23 -0500

    Pushed interview 10.

---
+++ src/aaronbearon/chapter10/interview/J110_12.java
@@ -0,0 +1,35 @@
+package aaronbearon.chapter10.interview;
+
+public class J110_12 {
+    public static void main(String[] args) {
+        CIRCLE circle = new CIRCLE();
+        for (double radius = 1; radius <= 100; radius *= 10) {
+            System.out.printf("Circle Radius: %.0f Area: %.4f%n", circle.getRadius(), circle.getArea());
+            circle.setRadius(radius * 10);
+        }
+    }
+}
+
+class CIRCLE {
+    private double radius;
+
+    CIRCLE() {
+        this(1);
+    }
+
+    CIRCLE(double radius) {
+        this.radius = radius;
+    }
+
+    void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    double getRadius() {
+        return radius;
+    }
+
+    double getArea() {
+        return radius * radius * Math.PI;
+    }
+}
