441560e925e43e2fa166acd16fb1cf80f4f66e2e
Aaron Blum <googolpup@gmail.com>
2026-01-26 15:27:09 -0500

    Interview 2 code

---
+++ src/aaronbearon/chapter11/interview/J202_1.java
@@ -0,0 +1,48 @@
+package aaronbearon.chapter11.interview;
+
+public class J202_1 {
+    public static void main(String[] args) {
+        String name = "Aaron";
+        double width = 8, height = 7;
+        System.out.println("Width: " + width + "\r\nHeight: " + height);
+        Rectangle r = new Rectangle(name, width, height);
+        System.out.println("Area: " + r.area());
+    }
+}
+
+class Shape {
+    private String creator;
+
+    public Shape(String creator) {
+        this.creator = creator;
+    }
+
+    public String getName() {
+        return creator;
+    }
+
+    public double area() {
+        System.out.println("Write code to calculate the area for each shape");
+        return 0.0; // default implementation }
+    }
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this("None", width, height);
+    }
+
+    public Rectangle(String creator, double width, double height) {
+        super(creator);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+}
