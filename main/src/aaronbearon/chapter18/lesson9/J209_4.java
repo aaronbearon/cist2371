236f3b59760badf99e132d9d3ac36e611f4ad79e
Aaron Blum <googolpup@gmail.com>
2026-04-02 15:46:10 -0400

    Pushed 4 and 5 before debugging

--- main/src/aaronbearon/chapter18/lesson9/J209_4.java
+++ main/src/aaronbearon/chapter18/lesson9/J209_4.java
@@ -1,26 +1,37 @@
 package aaronbearon.chapter18.lesson9;
 
+/**
+ * Aaron Blum, CIST 2372 Java 2, Lab 9 part 4
+ * Description: Write and test the binary search with recursion, without copying the textbook.
+ */
 public class J209_4 {
     public static void main(String[] args) {
+        // The testing array required by the lab instructions.
         int[] list = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
-        for (int i = 0; i <= list.length; i++) {
+        System.out.println("First array:");
+        for (int i = -1; i <= list.length; i++) {
             System.out.printf("Index of %d: %d%n", i, binarySearch(list, i));
         }
 
         System.out.println();
+        // Another array, but odd numbers can report negative indices.
         list = new int[]{20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0};
 
+        System.out.println("Second array:");
         for (int i = -1; i <= 21; i++) {
             System.out.printf("Index of %d: %d%n", i, binarySearch(list, i));
         }
     }
 
+    //* Collapse the variables into one return statement.
+    //* No need for list.length - 1
     public static int binarySearch(int[] list, int key) {
         return doBinarySearch(list, key, 0, list.length);
     }
 
     /**
+     * doBinarySearch performs the binary search
      *
      * @param list  The actual array being searched
      * @param key   The value being searched for
@@ -34,7 +45,12 @@ public class J209_4 {
         }
 
         int mid = (start + limit) / 2;
-        if ((mid < list.length - 1 && list[mid] <= list[mid + 1]) || (list[start] <= list[limit - 1] && start != limit - 1)) {
+
+        //* Sanity check the input list's ordering if we have enough elements.
+        if (mid != start && list[mid] >= list[start]) {
+            System.out.println("WARNING: Array not sorted or non-unique!");
+        }
+        if (mid != limit - 1 && list[mid] <= list[limit - 1]) {
             System.out.println("WARNING: Array not sorted or non-unique!");
         }
 
@@ -49,5 +65,16 @@ public class J209_4 {
 }
 
 /*
-TODO
+
+I made the binary search only work on arrays sorted descending order.
+
+I also added a detector against arrays that aren't truly descending.
+This only works on some numbers, but can alert the user about a sorting problem, unless it's non-existent.
+
+For example an array {12, 11, 10, 9, 8, 9, 7}.
+No problems can be detected while searching for 12, but 9 will detect a problem.
+
+Basically, since binary search only works on sorted arrays,
+    unsorted arrays can now be detected in some way.
+
 */
--- main/src/aaronbearon/chapter18/lesson9/J209_5.java
+++ main/src/aaronbearon/chapter18/lesson9/J209_5.java
@@ -2,6 +2,10 @@ package aaronbearon.chapter18.lesson9;
 
 import java.util.Scanner;
 
+/**
+ * Aaron Blum, CIST 2372 Java 2, Lab 9 part 5
+ * Description: Write and test the palindrome checker with recursion, without copying the textbook.
+ */
 public class J209_5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -13,22 +17,33 @@ public class J209_5 {
         }
     }
 
+    // Recursive helper method to define low and high
     public static boolean isPalindrome(String key) {
         if (key.isEmpty()) {
             return true;
         }
 
+        //* Start at middle and go outward.
         int low = (key.length() - 1) / 2;
-        int high = (key.length() - 1) - ((key.length() - 1) / 2);
+        int high = (key.length() - 1) - low;
         return isPalindrome(key, low, high);
     }
 
+    /**
+     * Check the string from the middle and gradually go outward to see if it's a palindrome.
+     *
+     * @param key  the string to check
+     * @param low  the char at the lowest index of current string portion
+     * @param high the char at the highest index of current string portion
+     * @return false, with mismatched chars
+     */
     public static boolean isPalindrome(String key, int low, int high) {
         if (low < 0) {
             return true;
         }
 
         if (key.charAt(low) == key.charAt(high)) {
+            //* Switch the increase and decrease for going middle to ends.
             return isPalindrome(key, low - 1, high + 1);
         } else {
             return false;
@@ -37,5 +52,8 @@ public class J209_5 {
 }
 
 /*
-TODO
+
+The textbook implementation starts at the ends of the string going inward.
+Mine starts at the middle of the string and goes outward.
+
 */
