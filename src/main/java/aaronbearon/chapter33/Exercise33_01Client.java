92913e9ca338fbd5cc85659b1485bda62cae9f72
Aaron Blum <googolpup@gmail.com>
2026-09-06 21:36:26 -0400

    Lab 3 done!

--- src/main/java/aaronbearon/chapter33/Exercise33_01Client.java
+++ src/main/java/aaronbearon/chapter33/Exercise33_01Client.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -14,12 +17,20 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * Aaron Blum, CIST 2373 Java 3, Lab 3, Client.
+ */
 public class Exercise33_01Client extends Application {
+    Socket socket = null;
     DataOutputStream toServer = null;
     DataInputStream fromServer = null;
 
+    TextArea taOutput;
+    TextField tfWeight;
+    TextField tfHeight;
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         final int SPACING = 5;
         GridPane gridPane = new GridPane();
         gridPane.setHgap(SPACING);
@@ -30,8 +41,8 @@ public class Exercise33_01Client extends Application {
         Label lblWeight = new Label("Weight in pounds");
         Label lblHeight = new Label("Height in inches");
 
-        TextField tfWeight = new TextField();
-        TextField tfHeight = new TextField();
+        tfWeight = new TextField();
+        tfHeight = new TextField();
         tfWeight.setAlignment(Pos.BASELINE_RIGHT);
         tfHeight.setAlignment(Pos.BASELINE_RIGHT);
         tfWeight.setPrefColumnCount(SPACING);
@@ -45,47 +56,60 @@ public class Exercise33_01Client extends Application {
         gridPane.add(tfHeight, 1, 1);
         gridPane.add(btnSubmit, 2, 1);
 
-        TextArea taOutput = new TextArea();
+        taOutput = new TextArea();
         taOutput.setEditable(false);
-        taOutput.setMinSize(400, 300);
-        taOutput.setMaxSize(400, 300);
-
-        ScrollPane scrollPane = new ScrollPane(taOutput);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
 
         BorderPane mainPane = new BorderPane();
         mainPane.setTop(gridPane);
-        mainPane.setCenter(scrollPane);
+        mainPane.setCenter(taOutput);
 
-        Scene scene = new Scene(mainPane, 300, 250);
-        primaryStage.setTitle("Exercise33_02Client");
+        Scene scene = new Scene(mainPane, 600, 400);
+        primaryStage.setTitle("Exercise33_01Client");
         primaryStage.setScene(scene);
         primaryStage.setResizable(true);
         primaryStage.show();
 
-        btnSubmit.setOnAction(_ -> {
-            try {
-                double weight = Double.parseDouble(tfWeight.getText());
-                double height = Double.parseDouble(tfHeight.getText());
-                toServer.writeDouble(weight);
-                toServer.writeDouble(height);
-                double BMI = fromServer.readDouble();
-                String status = fromServer.readUTF();
-                taOutput.clear();
-                taOutput.appendText("Weight: " + weight + "\r\n");
-                taOutput.appendText("Height: " + height + "\r\n");
-                taOutput.appendText("BMI: " + BMI + ". " + status + "\r\n");
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
+        btnSubmit.setOnAction(_ -> invokeBmi());
 
-        try (Socket socket = new Socket("localhost", 8888)) {
+        try {
+            socket = new Socket("localhost", 8000);
             fromServer = new DataInputStream(socket.getInputStream());
             toServer = new DataOutputStream(socket.getOutputStream());
         } catch (IOException ex) {
-            taOutput.appendText(ex + "\r\n");
+            // If we can't connect to the server, just die immediately.
+            // Nothing will work. Maybe in another lab we can write a loop
+            // to retry connecting to the server if it fails or breaks.
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        if (socket != null) {
+            socket.close();
+        }
+    }
+
+    /**
+     * Send weight and height to server.
+     * Receive BMI from server.
+     */
+    private void invokeBmi() {
+        try {
+            double weight = Double.parseDouble(tfWeight.getText());
+            double height = Double.parseDouble(tfHeight.getText());
+            toServer.writeDouble(weight);
+            toServer.writeDouble(height);
+            toServer.flush();
+            double bmi = fromServer.readDouble();
+            String status = fromServer.readUTF();
+            taOutput.clear();
+            taOutput.appendText(String.format("Weight is %.2f%n", weight));
+            taOutput.appendText(String.format("Height is %.2f%n", height));
+            taOutput.appendText(String.format("BMI is %.2f. %s%n", bmi, status));
+        } catch (Exception ex) {
+            System.out.println("DEBUG exception in client button");
+            taOutput.appendText(ex + String.format("%n"));
         }
     }
 
--- src/main/java/aaronbearon/chapter33/Exercise33_01Server.java
+++ src/main/java/aaronbearon/chapter33/Exercise33_01Server.java
@@ -1,35 +1,193 @@
 package aaronbearon.chapter33;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Exercise33_01Server extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        // Text area with a fixed minimum size
-        TextArea taOutput = new TextArea();
-        taOutput.setEditable(false);
-        taOutput.setMinWidth(400);
-        taOutput.setMaxWidth(400);
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-        // ScrollPane wrapping the text area underneath the top bar
-        ScrollPane scrollPane = new ScrollPane(taOutput);
-        scrollPane.setFitToHeight(true);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+/**
+ * Aaron Blum, CIST 2373 Java 3, Lab 3, Server.
+ */
+public class Exercise33_01Server extends Application {
+    ServerSocket serverSocket;
+    ExecutorService executor = Executors.newCachedThreadPool();
+
+    TextArea taOutput;
+
+    // We need a lock for open sockets so we can cleanly close sockets and sync up across threads.
+    final Object openSocketsLock = new Object();
+    Set<Socket> openSockets = new HashSet<>();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        taOutput = new TextArea();
+        taOutput.setEditable(false);
 
         BorderPane mainPane = new BorderPane();
-        mainPane.setCenter(scrollPane);
+        mainPane.setCenter(taOutput);
 
-        // Window scene setup
-        Scene scene = new Scene(mainPane, 300, 250);
-        primaryStage.setTitle("Exercise33_02CServer");
+        Scene scene = new Scene(mainPane, 600, 400);
+        primaryStage.setTitle("Exercise33_01Server");
         primaryStage.setScene(scene);
-        primaryStage.setResizable(true);
         primaryStage.show();
+
+        serverSocket = new ServerSocket(8000);
+        executor.submit(this::runSocketListener);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("Server shutting down");
+
+        // No new clients can join
+        if (serverSocket != null) {
+            serverSocket.close();
+        }
+        // Store sockets to close and nullify open sockets
+        Set<Socket> sockets;
+        synchronized (openSocketsLock) {
+            sockets = openSockets;
+            openSockets = null;
+        }
+        // Close sockets
+        for (Socket s : sockets) {
+            try {
+                s.close();
+            } catch (Exception e) {
+                // Ignore and continue closing sockets
+            }
+        }
+
+        executor.shutdown();
+        if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+            // After 1 minute of waiting, some thread must be blocked. This is a bug! Exit anyway.
+            System.out.println("Server exiting after timeout");
+            Runtime.getRuntime().exit(1);
+        }
+        System.out.println("Server stopped");
+    }
+
+    // Global UTC time format for logs.
+    private static final DateTimeFormatter logTimeFormatter =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").withZone(ZoneOffset.UTC);
+
+    // Logs a message into the log textbox.
+    private void log(String message) {
+        String timestamp = logTimeFormatter.format(Instant.now());
+        String text = timestamp + ": " + message + String.format("%n");
+        // Must run on UI thread.
+        Platform.runLater(() -> taOutput.appendText(text));
+    }
+
+    /**
+     * Loops forever on the server socket, accepting new incoming client connections.
+     * Each connection needs its own handler thread to run in.
+     */
+    public void runSocketListener() {
+        try {
+            log("Exercise33_01Server started at " + new Date());
+
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                log("Connected to a client at " + new Date());
+
+                // Add a socket unless the server is closing.
+                synchronized (openSocketsLock) {
+                    if (openSockets != null) {
+                        openSockets.add(socket);
+                    } else {
+                        // The server is already shutting down, just close the socket immediately.
+                        socket.close();
+                        continue;
+                    }
+                }
+
+                // Each client socket handler runs in its own thread.
+                // Make sure the socket is closed and removed from the
+                // openSockets list once the handler is done.
+                executor.submit(() -> {
+                    try {
+                        handleClientConnection(socket);
+                        socket.close();
+                    } catch (IOException e) {
+                        // Ignore, just making sure the socket is closed.
+                    } finally {
+                        synchronized (openSocketsLock) {
+                            if (openSockets != null) {
+                                openSockets.remove(socket);
+                            }
+                        }
+                    }
+                });
+            }
+        } catch (SocketException ex) {
+            // Ignore, server is shutting down
+        } catch (IOException ex) {
+            // This is unexpected
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Listen for weight and height from client, send BMI to client.
+     * Handle each incoming request, and send a response. Loop until the client
+     * closes the connection, or the server is shutting down.
+     */
+    public void handleClientConnection(Socket socket) {
+        try {
+            DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+            while (socket.isConnected()) {
+                double weight = inputFromClient.readDouble();
+                double height = inputFromClient.readDouble();
+                final double KILOGRAMS_PER_POUND = 0.45359237;
+                final double METERS_PER_INCH = 0.0254;
+                double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+                double heightInMeters = height * METERS_PER_INCH;
+                double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+                String status;
+                if (bmi < 18.5) {
+                    status = "Underweight";
+                } else if (bmi < 25) {
+                    status = "Normal";
+                } else if (bmi < 30) {
+                    status = "Overweight";
+                } else {
+                    status = "Obese";
+                }
+                outputToClient.writeDouble(bmi);
+                outputToClient.writeUTF(status);
+                outputToClient.flush();
+                log(String.format("Weight is %.2f", weight));
+                log(String.format("Height is %.2f", height));
+                log(String.format("BMI is %.2f. %s", bmi, status));
+            }
+        } catch (SocketException ex) {
+            // ignore, the server is probably closing or the client hung up.
+        } catch (IOException ex) {
+            // This is unexpected.
+            System.out.println("DEBUG exception in handler " + ex.getMessage());
+        } finally {
+            log("Client disconnected");
+        }
     }
 
     public static void main(String[] args) {
