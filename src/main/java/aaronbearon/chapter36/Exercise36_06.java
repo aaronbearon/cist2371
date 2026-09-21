4ce14ccf9c9a308c96cdc3467cbbc41c5940fec6
Aaron Blum <googolpup@gmail.com>
2026-09-21 10:33:24 -0400

    Lab 5 done!

--- src/main/java/aaronbearon/chapter36/Exercise36_06.java
+++ src/main/java/aaronbearon/chapter36/Exercise36_06.java
@@ -15,21 +15,40 @@ import javafx.stage.Stage;
 import java.text.NumberFormat;
 import java.util.Locale;
 
+/**
+ * Aaron Blum, CIST 2373 Java 3, Lab 5, Money Converter.
+ */
 public class Exercise36_06 extends Application {
-    private final TextField tfUSDollar = new TextField();
-    private final TextField a1 = new TextField();
-    private final TextField b1 = new TextField();
-    private final TextField a2 = new TextField();
-    private final TextField b2 = new TextField();
-    private final TextField a3 = new TextField();
-    private final TextField b3 = new TextField();
+    // Input field for entering original dollar amount.
+    private final TextField inputUsDollars = new TextField();
+
+    // Input fields to set the exchange rate for each country.
+    private final TextField[] exchangeInputs = new TextField[]{
+            new TextField(),
+            new TextField(),
+            new TextField(),
+    };
+
+    // Output fields to display the result currency for each country.
+    private final TextField[] countryOutputs = new TextField[]{
+            new TextField(),
+            new TextField(),
+            new TextField(),
+    };
+
+    // Each converter has an input, a country, and an output.
+    private final MoneyConverter[] converters = {
+            new MoneyConverter(exchangeInputs[0], countryOutputs[0], NumberFormat.getCurrencyInstance(Locale.CANADA)),
+            new MoneyConverter(exchangeInputs[1], countryOutputs[1], NumberFormat.getCurrencyInstance(Locale.FRANCE)),
+            new MoneyConverter(exchangeInputs[2], countryOutputs[2], NumberFormat.getCurrencyInstance(Locale.UK)),
+    };
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Outer most pane
+        // Outermost pane
         VBox vBox = new VBox(5);
 
-        // 1. Prompt for amount of  money
+        // 1. Prompt for amount of money.
         vBox.setAlignment(Pos.CENTER_LEFT);
         Label initDollar = new Label("Enter Dollar Amount");
 
@@ -39,16 +58,17 @@ public class Exercise36_06 extends Application {
 
         Label usMoney = new Label("US Dollars");
 
-        tfUSDollar.setAlignment(Pos.BASELINE_RIGHT);
-        tfUSDollar.setPrefWidth(285);
+        inputUsDollars.setAlignment(Pos.BASELINE_RIGHT);
+        inputUsDollars.setPrefWidth(285);
 
         Button convert = new Button("Convert");
 
-        hBox.getChildren().addAll(usMoney, tfUSDollar, convert);
+        hBox.getChildren().addAll(usMoney, inputUsDollars, convert);
 
-        // 3 Label for table
+        // 3. Label for table.
         Label displayExg = new Label("Display Exchange");
 
+        // 4. Grid pane.
         GridPane gridPane = getGridPane();
         vBox.getChildren().addAll(initDollar, hBox, displayExg, gridPane);
 
@@ -62,7 +82,7 @@ public class Exercise36_06 extends Application {
         primaryStage.show();
     }
 
-    // Builds the grid pane separately, using the given components
+    // Builds the grid pane separately, using the instance's text components.
     public GridPane getGridPane() {
         final int textFieldWidth = 150;
 
@@ -71,78 +91,55 @@ public class Exercise36_06 extends Application {
 
         Label exgRate = new Label("Exchange Rate");
         Label converted = new Label("Converted Amount");
-        Label canadian = new Label("Canadian Dollars");
-        Label euro = new Label("Euro");
-        Label british = new Label("British Pounds");
 
-        b1.setEditable(false);
-        b2.setEditable(false);
-        b3.setEditable(false);
-        a1.setAlignment(Pos.BASELINE_RIGHT);
-        b1.setAlignment(Pos.BASELINE_RIGHT);
-        a2.setAlignment(Pos.BASELINE_RIGHT);
-        b2.setAlignment(Pos.BASELINE_RIGHT);
-        a3.setAlignment(Pos.BASELINE_RIGHT);
-        b3.setAlignment(Pos.BASELINE_RIGHT);
-        a1.setPrefWidth(textFieldWidth);
-        b1.setPrefWidth(textFieldWidth);
-        a2.setPrefWidth(textFieldWidth);
-        b2.setPrefWidth(textFieldWidth);
-        a3.setPrefWidth(textFieldWidth);
-        b3.setPrefWidth(textFieldWidth);
+        Label[] countries = new Label[]{
+                new Label("Canadian Dollars"),
+                new Label("Euro"),
+                new Label("British Pounds"),
+        };
+
+        for (TextField tf : exchangeInputs) {
+            tf.setAlignment(Pos.BASELINE_RIGHT);
+            tf.setPrefWidth(textFieldWidth);
+        }
+
+        for (TextField tf : countryOutputs) {
+            tf.setAlignment(Pos.BASELINE_RIGHT);
+            tf.setPrefWidth(textFieldWidth);
+            tf.setEditable(false);
+        }
 
         gridPane.add(exgRate, 1, 0);
         gridPane.add(converted, 2, 0);
-        gridPane.add(canadian, 0, 1);
-        gridPane.add(a1, 1, 1);
-        gridPane.add(b1, 2, 1);
-        gridPane.add(euro, 0, 2);
-        gridPane.add(a2, 1, 2);
-        gridPane.add(b2, 2, 2);
-        gridPane.add(british, 0, 3);
-        gridPane.add(a3, 1, 3);
-        gridPane.add(b3, 2, 3);
+
+        for (int i = 0; i < 3; i++) {
+            gridPane.add(countries[i], 0, i + 1);
+            gridPane.add(exchangeInputs[i], 1, i + 1);
+            gridPane.add(countryOutputs[i], 2, i + 1);
+        }
         return gridPane;
     }
 
     // Converts the US dollar amount to the foreign currencies using the exchange rates.
     public void convert() {
-        double USDollars;
         // If the US Dollar amount contains invalid data, don't even attempt to convert.
         try {
-            USDollars = Double.parseDouble(tfUSDollar.getText());
-            USDollars = Math.round(USDollars * 100) / 100.0;
-        } catch (Exception e) {
-            tfUSDollar.setText("ERR");
-            return;
-        }
+            double usDollars = Double.parseDouble(inputUsDollars.getText());
+            usDollars = Math.round(usDollars * 100) / 100.0;
 
-        // Each converter has an input, a country, and an output.
-        MoneyConverter[] converters = {
-                new MoneyConverter(a1, b1, NumberFormat.getCurrencyInstance(Locale.CANADA)),
-                new MoneyConverter(a2, b2, NumberFormat.getCurrencyInstance(Locale.FRANCE)),
-                new MoneyConverter(a3, b3, NumberFormat.getCurrencyInstance(Locale.UK))
-        };
-
-        // Validates each input one at a time.
-        for (MoneyConverter converter : converters) {
-            if (isValidField(converter.exgRate(), converter.convertedRate())) {
-                double exchangeRate = Double.parseDouble(converter.exgRate().getText());
-                converter.convertedRate().setText(converter.currencyFormat().format(USDollars * exchangeRate));
+            // Validates each input one at a time.
+            for (MoneyConverter converter : converters) {
+                try {
+                    double exchangeRate = Double.parseDouble(converter.exgRate().getText());
+                    converter.convertedRate().setText(converter.currencyFormat().format(usDollars * exchangeRate));
+                } catch (Exception e) {
+                    converter.exgRate().setText("ERR");
+                    converter.convertedRate().clear();
+                }
             }
-        }
-    }
-
-    // If the input field is invalid, display "ERR" in the field and clear the output field.
-    public boolean isValidField(TextField fieldIn, TextField fieldOut) {
-        try {
-            Double.parseDouble(fieldIn.getText());
         } catch (Exception e) {
-            fieldIn.setText("ERR");
-            fieldOut.clear();
-            return false;
+            inputUsDollars.setText("ERR");
         }
-        return true;
     }
 
     public static void main(String[] args) {
